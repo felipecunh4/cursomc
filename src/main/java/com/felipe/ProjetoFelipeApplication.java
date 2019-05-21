2b46ba0de467831875ade2c8ff6becc6e3c9399f
Felipe Cunha <fnasccunha@gmail.com>
2019-05-21 11:19:36 -0300

    Projeto criado

---
+++ src/main/java/com/felipe/ProjetoFelipeApplication.java
@@ -0,0 +1,13 @@
+package com.felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFelipeApplication.class, args);
+	}
+
+}
