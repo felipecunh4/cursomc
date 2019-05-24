9d5e5e390364d7c257eb133d90ed2819385618ad
Felipe Cunha <felipe.n.cunha@accenture.com>
2019-05-24 08:59:59 -0300

    validacao com bean validation

--- src/main/java/com/felipe/services/CategoriaService.java
+++ src/main/java/com/felipe/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipe.domain.Categoria;
+import com.felipe.dto.CategoriaDTO;
 import com.felipe.exceptions.DataIntegrityException;
 import com.felipe.exceptions.ObjectNotFoundException;
 import com.felipe.repositories.CategoriaRepository;
@@ -52,9 +53,14 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return repo.findAll(); 
 	}
+	
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);	
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
