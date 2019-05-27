e66ac8d22baca41caf1f0e9a6bdc8b13e011d453
Felipe Cunha <felipe.n.cunha@accenture.com>
2019-05-27 09:40:58 -0300

    validando email do Cliente INSERT

--- src/main/java/com/felipe/repositories/ClienteRepository.java
+++ src/main/java/com/felipe/repositories/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.felipe.repositories;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.felipe.domain.Cliente;
 
@@ -8,5 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 	
 }
--- src/main/java/com/felipe/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/felipe/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.felipe.domain.Cliente;
 import com.felipe.domain.enums.TipoCliente;
 import com.felipe.dto.ClienteNewDTO;
+import com.felipe.repositories.ClienteRepository;
 import com.felipe.resources.exception.FieldMessage;
 import com.felipe.services.validation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -26,6 +33,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente."));
 		}
 
 		for (FieldMessage e : list) {
